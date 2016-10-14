6ba4c2bb1432ed0c734afa5ad7c52cc6ca1b0525
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-10-14 15:56:17 +0100

    Make sure the converge works

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -20,11 +20,10 @@ require 'spec_helper'
 
 describe 'reapr::default' do
   context 'When all attributes are default, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
+    # let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+    let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
 
+    # Make sure the converge works
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
